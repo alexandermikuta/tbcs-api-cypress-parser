b8136836180bc5501189047b1c00f31ab0e46546
HolgerAtImbus <holger.seidel@imbus.de>
2021-11-01 14:27:01 +0100

    added a simple log level to the report logger

--- example/cypress/support/tbcs/report.logger.ts
+++ example/cypress/support/tbcs/report.logger.ts
@@ -1,22 +1,33 @@
 import moment = require("moment");
 
+export enum LogLevel {
+  DEBUG = 4,
+  INFO = 3,
+  WARN = 2,
+  ERROR = 1
+}
 export class ReportLogger {
   private static TEST_RESULT_FOLDER = 'test-results';
+  private static LOG_LEVEL: LogLevel = LogLevel.INFO;
+
+  public static setlevel(level: LogLevel) {
+    ReportLogger.LOG_LEVEL = level;
+  }
 
   public static info(msg: string) {
-    ReportLogger.log('INFO', msg);
+    ReportLogger.LOG_LEVEL >= LogLevel.INFO ? ReportLogger.log('INFO', msg) : null;
   }
 
   public static debug(msg: string) {
-    ReportLogger.log('DEBUG', msg);
+    ReportLogger.LOG_LEVEL >= LogLevel.DEBUG ?ReportLogger.log('DEBUG', msg):null;
   }
 
   public static warn(msg: string) {
-    ReportLogger.log('WARN', msg);
+    ReportLogger.LOG_LEVEL >= LogLevel.WARN ?ReportLogger.log('WARN', msg):null;
   }
 
   public static error(msg: string) {
-    ReportLogger.log('ERROR', msg);
+    ReportLogger.LOG_LEVEL >= LogLevel.ERROR ?ReportLogger.log('ERROR', msg): null;
   }
 
   private static log(level: string, msg: string) {
